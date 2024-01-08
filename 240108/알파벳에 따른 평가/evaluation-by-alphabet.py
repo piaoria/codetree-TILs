2f21b59152c16b6c089f5a254c5671dac93b11c0
piaoria <155702981+piaoria@users.noreply.github.com>
2024-01-08 21:53:33 +0900

    Title: 알파벳에 따른 평가, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240108/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+en = input()
+
+if en == "S":
+    print("Superior")
+elif en == "A":
+    print("Excellent")
+elif en == "B":
+    print("Good")
+elif en == "C":
+    print("Usually")
+elif en == "D":
+    print("Effort")
+else:
+    print("Failure")
