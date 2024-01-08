aa881ff5ab5ca21b9eeee27ebe18bab74e5a97f4
piaoria <155702981+piaoria@users.noreply.github.com>
2024-01-09 08:40:01 +0900

    Title: 두 숫자의 짝홀 여부, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240109/두 숫자의 짝홀 여부/parity-of-two-numbers.py
@@ -0,0 +1,16 @@
+arr = input().split()
+
+a = int(arr[0])
+b = int(arr[1])
+
+if a % 2 == 0:
+    print("even")
+else:
+    b % 2 == 1
+    print("odd")
+
+if b % 2 == 0:
+    print("even")
+else: 
+    b % 2 == 1
+    print("odd")
