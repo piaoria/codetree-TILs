e24f7bab8878c11fcd51996fd259e5e3b618b656
piaoria <155702981+piaoria@users.noreply.github.com>
2024-01-10 13:06:05 +0900

    Title: 두 사람, Time: 157ms, Memory: 24MB, Status: Passed - CodeTree

--- 240110/두 사람/two-person.py
+++ 240110/두 사람/two-person.py
@@ -2,8 +2,8 @@ arr_a = input().split()
 arr_b = input().split()
 
 a_age = int(arr_a[0])
-a_gen = (arr_a[1])
+a_gen = arr_a[1]
 b_age = int(arr_b[0])
-b_gen = (arr_b[1])
+b_gen = arr_b[1]
 
-print(int((a_age >= 19 or b_age >= 19) and (a_gen == "M" or b_gen == "M")))
+print(int((a_age >= 19 and a_gen == "M") or (b_age >= 19 and b_gen == "M")))
