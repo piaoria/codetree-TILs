959bbecb7493b5be5fc45f96a4a15ab6efbd471c
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-17 23:59:05 +0900

    Title: 세 실수의 반올림, Time: 169ms, Memory: 14MB, Status: Runtime Error - Codetree

--- 250417/세 실수의 반올림/rounding-of-three-actual-numbers.js
+++ 250417/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -2,10 +2,6 @@ const fs = require("fs");
 
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-let a = Number(input[0]);
-let b = Number(input[1]);
-let c = Number(input[2]);
-
-console.log(a.toFixed(3));
-console.log(b.toFixed(3));
-console.log(c.toFixed(3));
+for (let i = 0; i < 3; i++ ) {
+    console.log(input[i].toFixed(3))
+}
