dfcf3b30a0ba9aa66d4aaa8b06bf29915a274d5e
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-18 00:15:27 +0900

    Title: 합과 평균, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

--- 250418/합과 평균/sum-and-avg.js
+++ 250418/합과 평균/sum-and-avg.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-const input_data = fs.readFileSync(0).toString().trim().split();
+const input_data = fs.readFileSync(0).toString().trim().split(" ");
 
-const sum_num = Number(input_data[0])+Number(input_data[1])
+const sum_num = Number(input_data[0])  +Number(input_data[1])
 
 console.log(sum_num, ((sum_num / (input_data).length)).toFixed(1));
