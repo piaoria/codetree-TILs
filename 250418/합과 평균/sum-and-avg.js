1c547c8ba01c9042b64bebae76142b613da6e6db
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-18 00:13:03 +0900

    Title: 합과 평균, Time: 165ms, Memory: 14MB, Status: Runtime Error - Codetree

--- 250418/합과 평균/sum-and-avg.js
+++ 250418/합과 평균/sum-and-avg.js
@@ -4,4 +4,4 @@ const input_data = fs.readFileSync(0).toString().trim().split();
 
 const sum_num = Number(input_data[0])+Number(input_data[1])
 
-console.log((sum_num, sum_num / length(input_data)).toFixed(2))
+console.log((sum_num, (sum_num / length(input_data).toFixed)));
