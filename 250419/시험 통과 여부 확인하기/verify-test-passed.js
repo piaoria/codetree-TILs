1aa51eaccc54313f1926ba440142a33dda9d19ec
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-19 11:42:49 +0900

    Title: 시험 통과 여부 확인하기, Time: 195ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250419/시험 통과 여부 확인하기/verify-test-passed.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const inputScore = Number(fs.readFileSync(0).toString().trim())
+
+function printExamResult(score) {
+    if (score >= 0 && score <= 100) {
+        console.log("pass")
+    } else {
+        console.log(`${100 - score} more score`)
+    }
+}
+
+printExamResult(inputScore)
