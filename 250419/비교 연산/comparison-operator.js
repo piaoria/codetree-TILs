f2dd1a71883f3e69546476adf3dbbe60cafa3910
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-19 10:33:16 +0900

    Title: 비교 연산, Time: 175ms, Memory: 15MB, Status: Passed - Codetree

--- 250419/비교 연산/comparison-operator.js
+++ 250419/비교 연산/comparison-operator.js
@@ -6,41 +6,12 @@ let a = Number(input_data[0])
 let b = Number(input_data[1])
 
 function judge(num_1, num_2) {
-    if (num_1>=num_2) {
-        console.log(1)
-    } else {
-        console.log(0)
-    }
-
-    if (num_1>num_2) {
-        console.log(1)
-    } else {
-        console.log(0)
-    }
-
-    if (num_1<=num_2) {
-        console.log(1)
-    } else {
-        console.log(0)
-    }
-
-    if (num_1<num_2) {
-        console.log(1)
-    } else {
-        console.log(0)
-    }
-
-    if (num_1 === num_2) {
-        console.log(1)
-    } else {
-        console.log(0)
-    }
-
-    if (num_1 !== num_2) {
-        console.log(1)
-    } else {
-        console.log(0)
-    }
+    console.log(Number(num_1>=num_2))
+    console.log(Number(num_1>num_2))
+    console.log(Number(num_1<=num_2))
+    console.log(Number(num_1<num_2))
+    console.log(Number(num_1===num_2))
+    console.log(Number(num_1!==num_2))
 }
 
 judge(a, b)
