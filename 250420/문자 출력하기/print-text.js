1bc7453180c18390c772d5ece142f1fd5b21297b
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-20 14:38:31 +0900

    Title: 문자 출력하기, Time: 170ms, Memory: 15MB, Status: Passed - Codetree

--- 250420/문자 출력하기/print-text.js
+++ 250420/문자 출력하기/print-text.js
@@ -3,13 +3,9 @@ const fs = require("fs");
 const inputWord = fs.readFileSync(0).toString().trim();
 
 function printEightTimesRepeat (word) {
-    let result = "";
-
     for (i = 0; i < 8; i ++) {
-        result += word
+        process.stdout.write(word);
     };
-
-    return result;
 }
 
-console.log(printEightTimesRepeat(inputWord));
+printEightTimesRepeat(inputWord);
