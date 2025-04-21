65caa6a0e43bc2ddc57816cf4503f2b187b781c4
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-21 23:49:38 +0900

    Title: 점수 비교, Time: 191ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250421/점수 비교/score-comparison.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+const inputData = fs.readFileSync(0).toString().trim().split("\n");
+
+const inputScoreA = inputData[0].split(" ");
+let mathScoreA = Number(inputScoreA[0]);
+let engScoreA = Number(inputScoreA[1]);
+
+const inputScoreB = inputData[1].split(" ");
+let mathScoreB = Number(inputScoreB[0]);
+let engScoreB = Number(inputScoreB[1]);
+
+function compareHighScore(mathA, engA, mathB, engB) {
+    if ((mathA > mathB) && (engA > engB)) {
+        return 1;
+    } else {
+        return 0;
+    };
+};
+
+console.log(compareHighScore(mathScoreA, engScoreA, mathScoreB, engScoreB))
