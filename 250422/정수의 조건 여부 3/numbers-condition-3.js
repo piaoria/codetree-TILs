d1f9a7d773f06a694123a158cda5d7c47b1d1131
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-22 00:08:47 +0900

    Title: 정수의 조건 여부 3, Time: 169ms, Memory: 15MB, Status: Passed - Codetree

--- 250422/정수의 조건 여부 3/numbers-condition-3.js
+++ 250422/정수의 조건 여부 3/numbers-condition-3.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 const inputA = Number(fs.readFileSync(0).toString().trim());
 
 function isMultipleThirteenOrNineteen(value) {
-    if (((value / 13) - Number((value / 13).toFixed(0)) > 0) && ((value / 19) - Number((value / 19).toFixed(0)) > 0)) {
-        return "False";
-    } else {
+    if (((value / 13) - Number((value / 13).toFixed(0)) == 0) || ((value / 19) - Number((value / 19).toFixed(0)) == 0)) {
         return "True";
+    } else {
+        return "False";
     };
 };
 
