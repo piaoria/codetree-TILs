a90f544d754ed3f1672fb70f000e2e2e1c067015
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-24 00:38:44 +0900

    Title: 윤년인가, Time: 167ms, Memory: 15MB, Status: Passed - Codetree

--- 250424/윤년인가/is-leap-year.js
+++ 250424/윤년인가/is-leap-year.js
@@ -1,7 +1,3 @@
 const inputYear = parseInt(require("fs").readFileSync(0).toString().trim(), 10);
 
-function isLeapYear(year) {
-    return year % 4 === 0 ? (year % 100 === 0) && (year % 400 !== 0) ? "false" : "true" : "false";
-};
-
-console.log(isLeapYear(inputYear));
+console.log(inputYear % 4 === 0 ? (inputYear % 100 === 0) && (inputYear % 400 !== 0) ? "false" : "true" : "false");
