e6b9eb74a459c628acf29709e3836b4ea49388b7
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-23 23:15:28 +0900

    Title: 굉장한 수, Time: 162ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250423/굉장한 수/amazing-number.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const inputNum = Number(fs.readFileSync(0).toString().trim());
+
+function isGreatNumber(num) {
+    if ((num % 2 == 1) && (num % 3 == 0)) {
+        return "true"
+    } else if ((num % 2 == 0) && (num % 5 == 0)) {
+        return "true"
+    } else {
+        return "false"
+    };
+};
+
+console.log(isGreatNumber(inputNum));
