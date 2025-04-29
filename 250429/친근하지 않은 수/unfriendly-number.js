25c1130d6f4f2b4a2f09193d4965deda8f963b83
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-29 17:12:40 +0900

    Title: 친근하지 않은 수, Time: 187ms, Memory: 15MB, Status: Passed - Codetree

--- 250429/친근하지 않은 수/unfriendly-number.js
+++ 250429/친근하지 않은 수/unfriendly-number.js
@@ -5,7 +5,7 @@ const inputNum = Number(fs.readFileSync(0).toString().trim());
 function getNotFriendlyNumberList(num) {
     let resultArray = [];
 
-    for (let i = 1; i < num; i ++) {
+    for (let i = 1; i < num + 1; i ++) {
         if (i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0) {
             resultArray.push(i);
         }
