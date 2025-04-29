9756ae7ca787ea4ae376ceb8df99245540f60b58
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-29 09:41:26 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 170ms, Memory: 15MB, Status: Passed - Codetree

--- 250429/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.js
+++ 250429/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.js
@@ -4,16 +4,8 @@ const [inputInt, inputArray] = fs.readFileSync(0).toString().trim().split("\n");
 
 let numberList = inputArray.split(" ").map(Number);
 
-function getEvenNumberReverseArray(len,array) {
-    let result = [];
-
-    for (let i = 0; i < len; i ++) {
-        if (array[i] % 2 == 0) {
-            result.unshift(array[i]);
-        };
-    };
-
-    return result;
+function getEvenNumber(num) {
+    return num % 2 == 0;
 };
 
-console.log(getEvenNumberReverseArray(inputInt,numberList).join(" "));
+console.log(numberList.filter(getEvenNumber).reverse().join(" "));
