86c077173d46d81eba2b4bc408e5d5f48509a0c6
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-04-29 17:41:25 +0900

    Title: 배수의 개수, Time: 180ms, Memory: 15MB, Status: Passed - Codetree

--- 250429/배수의 개수/number-of-multipliers.js
+++ 250429/배수의 개수/number-of-multipliers.js
@@ -3,18 +3,19 @@ const fs = require("fs");
 const inputArray = fs.readFileSync(0).toString().trim().split("\n").map(Number);
 
 function getThreeOrFiveMultipleList(array) {
-    let resultThreeList = [];
-    let resultFiveList = [];
+    let resultThreeCnt = 0;
+    let resultFiveCnt = 0;
 
     for (let i = 0; i < array.length; i ++) {
         if (array[i] % 3 === 0) {
-            resultThreeList.push(array[i]);
-        } if (array[i] % 5 === 0) {
-            resultFiveList.push(array[i]);
+            resultThreeCnt ++
+        };
+        if (array[i] % 5 === 0) {
+            resultFiveCnt ++
         };
     };
 
-    return resultThreeList, resultFiveList;
+    return [resultThreeCnt, resultFiveCnt];
 };
 
-console.log(getThreeOrFiveMultipleList(inputArray));
+console.log(getThreeOrFiveMultipleList(inputArray).join(" "));
