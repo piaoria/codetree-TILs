ebcf9899960ef3ba511d4f45f1957b2517714257
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-05-03 00:38:21 +0900

    Title: 배열의 값을 3배로, Time: 178ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250503/배열의 값을 3배로/triple-the-values-of-the-array.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const inputMatrix = fs.readFileSync(0).toString().trim().split("\n");
+
+function makeNewMatrix(initMatrix) {
+    
+    return matrix = initMatrix.map(line => line.split(" ").map(num => num * 3));
+};
+
+const newMatrix = makeNewMatrix(inputMatrix);
+
+for (let i = 0; i < newMatrix.length; i ++) {
+    console.log(newMatrix[i].join(" "));
+};
