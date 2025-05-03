ea2852087ffb20232d7c0743bf4fc43b054e1437
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-05-04 00:27:45 +0900

    Title: 좌우로 반복하며 출력, Time: 173ms, Memory: 14MB, Status: Runtime Error - Codetree

---
+++ 250504/좌우로 반복하며 출력/print-in-left-right-reverse.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+const inputNum = Number(fs.readFileSync(0).toString().trim());
+
+function makeNewArray(intN) {
+    let result = [];
+    let line = [];
+
+    for (let i = 1; i <= intN; i ++) {
+        line.push(i);
+    };
+
+    for (let j = 0; j < intN; j ++) {
+        if (j % 2 === 1) {
+            result.push([...line].reverse());
+        } else {
+            result.push(line);
+        };
+    };
+
+    return result;
+};
+
+const newArray = makeNewArray(inputNum);
+
+for (let row = 0; row < 4; row ++) {
+    console.log(newArray[row].join(""));
+};
