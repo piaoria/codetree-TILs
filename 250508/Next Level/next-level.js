7de1894fb2be129496680c970ba4bad4f8f4477e
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-05-08 01:16:52 +0900

    Title: Next Level, Time: 189ms, Memory: 15MB, Status: Passed - Codetree

--- 250508/Next Level/next-level.js
+++ 250508/Next Level/next-level.js
@@ -6,10 +6,15 @@ class UserInfo {
         this.id = id;
         this.level = level;
     }
+    
+    printUserInfo(){
+        console.log(`user ${this.id} lv ${this.level}`);
+    }
+
 }
 
 const userInfo1 = new UserInfo("codetree", "10");
 const userInfo2 = new UserInfo(...inputData);
 
-console.log(`user ${userInfo1.id} lv ${userInfo1.level}`)
-console.log(`user ${userInfo2.id} lv ${userInfo2.level}`)
+userInfo1.printUserInfo()
+userInfo2.printUserInfo()
