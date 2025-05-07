f27d70827c114344ff01b911a95c90831b000b55
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-05-07 21:48:05 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 172ms, Memory: 15MB, Status: Passed - Codetree

--- 250507/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.js
+++ 250507/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.js
@@ -5,7 +5,7 @@ const get100Sequence = (intN) => {
     if (intN === 1) return 2;
     else if (intN === 2) return 4;
 
-    return (get100Sequence(intN - 1) * get100Sequence(intN - 2));
+    return ((get100Sequence(intN - 1) * get100Sequence(intN - 2)) % 100);
 };
 
-console.log((get100Sequence(inputNum)) % 100);
+console.log(get100Sequence(inputNum));
