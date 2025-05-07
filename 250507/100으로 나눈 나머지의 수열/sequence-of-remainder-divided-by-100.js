e54eff900963869b4010eda7f068a9f80099816a
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-05-07 21:46:43 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 187ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250507/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const inputNum = Number(fs.readFileSync(0).toString().trim());
+
+const get100Sequence = (intN) => {
+    if (intN === 1) return 2;
+    else if (intN === 2) return 4;
+
+    return (get100Sequence(intN - 1) * get100Sequence(intN - 2));
+};
+
+console.log((get100Sequence(inputNum)) % 100);
