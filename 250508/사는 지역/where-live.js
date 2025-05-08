630e0940f4eebe41525f9fb17a7d7c8d1c7ddb5b
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-05-08 23:07:45 +0900

    Title: 사는 지역, Time: 187ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250508/사는 지역/where-live.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const inputData = fs.readFileSync(0).toString().trim().split('\n');
+
+const intN = Number(inputData[0]);
+const personLines = inputData.slice(1, intN + 1);
+
+class Person {
+    constructor(name, addr, city) {
+        this.name = name;
+        this.addr = addr;
+        this.city = city;
+    }
+
+    printPersonInfo() {
+        console.log(`name ${this.name}`);
+        console.log(`addr ${this.addr}`);
+        console.log(`city ${this.city}`);
+    }
+}
+
+const personList = [];
+for (let person of personLines) {
+    personList.push(new Person(...person.split(" ")));
+}
+
+personList.sort(this.name)[intN - 1].printPersonInfo();
