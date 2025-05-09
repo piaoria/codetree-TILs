c1c954ca48a35231def6026b0e73bdb978c7b918
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-05-09 20:02:43 +0900

    Title: 총점 비교, Time: 172ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250509/총점 비교/compare-total-points.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const inputData = fs.readFileSync(0).toString().trim().split("\n");
+const inputStudentsInfo = inputData.slice(1);
+
+class StudentGrade {
+    constructor(name, score1, score2, score3) {
+        this.name = name;
+        this.score1 = Number(score1);
+        this.score2 = Number(score2);
+        this.score3 = Number(score3);
+    }
+
+    printStudentInfo() {
+        console.log(this.name, this.score1, this.score2, this.score3);
+    }
+}
+
+const studentsGradeList = inputStudentsInfo.map((line) => {
+    const [name, score1, score2, score3] = line.split(" ")
+
+    return new StudentGrade(name, score1, score2, score3);
+});
+
+const getSumGrade = (grades) => {
+
+    return grades.score1 + grades.score2 + grades.score3;
+};
+
+const sumCmp = (prev, cur) => {
+    
+    return getSumGrade(prev) - getSumGrade(cur);
+};
+
+studentsGradeList.sort((prev, cur) => sumCmp(prev, cur));
+
+studentsGradeList.forEach((line) => line.printStudentInfo());
