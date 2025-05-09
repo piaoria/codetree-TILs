e4a1c529efe9429df071f2f594a6aa8658caf1ba
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-05-10 00:22:39 +0900

    Title: 개인정보, Time: 219ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250510/개인정보/personal-info.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const inputProfileData = fs.readFileSync(0).toString().trim().split("\n");
+
+class Profile {
+    constructor(name, height, weight) {
+        this.name = name;
+        this.height = Number(height);
+        this.weight = Number(weight);
+    }
+
+    printProfileInfo() {
+        console.log(this.name, this.height, Math.floor(this.weight * 10) / 10);
+    }
+}
+
+const profileList = inputProfileData.map((data) => {
+    const [name, height, weight] = data.split(" ");
+
+    return new Profile(name, height, weight);
+});
+
+const cmpHeight = (prev, cur) => {
+    return cur.height - prev.height;
+};
+
+const sortedNameList = [...profileList].sort((prev, cur) => prev.name.localeCompare(cur.name));
+const sortedHeightList = [...profileList].sort((prev, cur) => cmpHeight(prev, cur));
+console.log("name");
+sortedNameList.forEach((info) => info.printProfileInfo());
+console.log();
+console.log("height");
+sortedHeightList.forEach((info) => info.printProfileInfo());
