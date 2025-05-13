527539c70b28a185d0175867c7d7786522f7d02b
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-05-14 02:29:58 +0900

    Title: 여러가지 진수변환, Time: 189ms, Memory: 15MB, Status: Passed - Codetree

--- 250514/여러가지 진수변환/various-numeral-system-transformations.js
+++ 250514/여러가지 진수변환/various-numeral-system-transformations.js
@@ -1,38 +1,21 @@
 const fs = require("fs");
 const [intN, baseInt] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-const convertBase4 = (num) => {
-    let base4Array = [];
+const convertBase = (num, target) => {
+    let baseTargetArray = [];
 
     while (true) {
-        if (Math.trunc(num / 4) === 0) {
-            base4Array.unshift(num % 4);
+        if (Math.trunc(num / target) === 0) {
+            baseTargetArray.unshift(num % target);
             break;
         }
 
-        base4Array.unshift(num % 4);
-        num = Math.trunc(num / 4);
+        baseTargetArray.unshift(num % target);
+        num = Math.trunc(num / target);
 
     }
 
-    return base4Array.join("");
+    return baseTargetArray.join("");
 };
 
-const convertBase8 = (num) => {
-    let base8Array = [];
-
-    while (true) {
-        if (Math.trunc(num / 8) === 0) {
-            base8Array.unshift(num % 8);
-            break;
-        }
-
-        base8Array.unshift(num % 8);
-        num = Math.trunc(num / 8);
-
-    }
-
-    return base8Array.join("");  
-};
-
-console.log(baseInt === 4 ? convertBase4(intN) : convertBase8(intN));
+console.log(convertBase(intN, baseInt));
