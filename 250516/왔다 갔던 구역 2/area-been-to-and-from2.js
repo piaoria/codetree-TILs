9f7a38e0fd5bba72c8e2eb212aa3b7a934821b99
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-05-16 00:19:40 +0900

    Title: 왔다 갔던 구역 2, Time: 195ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250516/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const inputData = fs.readFileSync(0).toString().trim().split("\n");
+
+const intN = Number(inputData[0]);
+const inputCommandData = inputData.slice(1).map((command) => command.split(" "));
+
+const area = new Array(2 * intN * 10 + 1).fill(0);
+let originIndex = intN * 10;
+
+const movePosition = (commandList, idx) => {
+    for (let com of commandList) {
+        const x = Number(com[0]);
+        const direct = com[1];
+
+        if (direct === "R") {
+            for (let i = idx; i < idx + x; i ++) {
+                area[i] ++;
+            }
+            idx += x;
+
+        } else if (direct === "L") {
+            for (let j = idx; j > idx - x; j --) {
+                area[j] ++;
+            }
+            idx -= x;
+
+        }
+
+    }
+};
+
+movePosition(inputCommandData, originIndex);
+
+let count = 0;
+
+area.forEach((factor) => {if (factor >= 2) count ++});
+console.log(count);
