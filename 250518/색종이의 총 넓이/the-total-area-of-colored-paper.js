fc31aca542f93fece72d9956901ba6b7bb6c78e8
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-05-18 01:10:49 +0900

    Title: 색종이의 총 넓이, Time: 177ms, Memory: 15MB, Status: Passed - Codetree

--- 250518/색종이의 총 넓이/the-total-area-of-colored-paper.js
+++ 250518/색종이의 총 넓이/the-total-area-of-colored-paper.js
@@ -7,6 +7,17 @@ const inputPaperList = inputData.slice(1).map((list) => list.split(" ").map((fac
 
 const grid = Array.from({length : 201}, () => Array(201).fill(0));
 
+let minX = 201, minY = 201;
+let maxX = 0, maxY = 0;
+
+for (let paper of inputPaperList) {
+    const [x, y] = paper;
+    minX = Math.min(minX, x);
+    minY = Math.min(minY, y);
+    maxX = Math.max(maxX, x + 8);
+    maxY = Math.max(maxY, y + 8);
+}
+
 const fillSquare64Extent = (paperList) => {
     for (let paper of paperList) {
         let paperX = paper[0];
@@ -24,8 +35,8 @@ fillSquare64Extent(inputPaperList);
 
 let dimension = 0;
 
-for (let i = 0; i < 201; i ++) {
-    for (let j = 0; j < 201; j ++) {
+for (let i = minX; i < maxX; i ++) {
+    for (let j = minY; j < maxY; j ++) {
         if (grid[i][j] === 1) {
             dimension ++;
         }
