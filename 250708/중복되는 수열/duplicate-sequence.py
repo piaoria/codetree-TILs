02754d3788aec0fd28721f9fd11179e275a6dfdf
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-07-08 23:34:31 +0900

    Title: 중복되는 수열, Time: 111ms, Memory: 29MB, Status: Passed - Codetree

---
+++ 250708/중복되는 수열/duplicate-sequence.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+array_list = [input().strip() for _ in range(n)]
+
+class Node:
+    def __init__(self):
+        self.is_end = False
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, number):
+        node = self.root
+
+        for num in number:
+            if node.is_end:
+                return True
+            if num not in node.children:
+                node.children[num] = Node()
+            node = node.children[num]
+        node.is_end = True
+        return False
+
+
+result = True
+trie = Trie()
+
+for nm in array_list:
+    if trie.insert(nm):
+        result = False
+        break
+print(int(result))
