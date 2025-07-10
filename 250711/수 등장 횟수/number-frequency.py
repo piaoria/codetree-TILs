5b7ac3c8d4c2a91cafb50c340d0f67c2ad1e3975
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-07-11 07:04:26 +0900

    Title: 수 등장 횟수, Time: 122ms, Memory: 28MB, Status: Passed - Codetree

--- 250711/수 등장 횟수/number-frequency.py
+++ 250711/수 등장 횟수/number-frequency.py
@@ -1,5 +1,5 @@
 import sys
-from collections import defaultdict
+from collections import Counter
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
@@ -7,10 +7,7 @@ n, m = map(int, input().split())
 num_list = list(map(int, input().split()))
 test_list = list(map(int, input().split()))
 
-counter = defaultdict(int)
-
-for num in num_list:
-    counter[num] += 1
+counter = Counter(num_list)
 
 for test in test_list:
     print(counter[test], end =" ")
