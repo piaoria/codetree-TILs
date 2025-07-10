08b3573eb2061b4f8fc7104d25c17ad7ed3c487e
CodingLarva <155702981+piaoria@users.noreply.github.com>
2025-07-11 07:44:41 +0900

    Title: 가장 많은 데이터, Time: 141ms, Memory: 32MB, Status: Passed - Codetree

---
+++ 250711/가장 많은 데이터/most-frequent-data.py
@@ -0,0 +1,13 @@
+import sys
+from collections import Counter
+
+input = sys.stdin.readline
+n = int(input())
+
+text_list = [input().strip() for _ in range(n)]
+counter = Counter(text_list)
+
+value_list = list(counter.values())
+value_list.sort()
+
+print(value_list[-1])
